7129e93c554283724a6206cd1a219630087a26ff
Waenara <148997679+Waenara@users.noreply.github.com>
2024-01-03 21:24:16 +0200

    фикс бага

--- src/main/java/f3f5/spawnauth/Events.java
+++ src/main/java/f3f5/spawnauth/Events.java
@@ -3,6 +3,7 @@ package f3f5.spawnauth;
 import fr.xephi.authme.api.v3.AuthMeApi;
 import fr.xephi.authme.events.LoginEvent;
 import fr.xephi.authme.events.LogoutEvent;
+import fr.xephi.authme.events.UnregisterByPlayerEvent;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -80,6 +81,18 @@ public class Events implements Listener {
     private void OnLogout(LogoutEvent event) {
         Player player = event.getPlayer();
         helpers.cacheOriginalLocation(player.getName(), player.getLocation());
-        helpers.teleportAway(player);
+    }
+    @EventHandler(priority = EventPriority.NORMAL)
+    private void onUnregisterByPlayer(UnregisterByPlayerEvent event) {
+        Player player = event.getPlayer();
+        helpers.cacheOriginalLocation(player.getName(), player.getLocation());
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    private void onUnregisterByAdmin(UnregisterByPlayerEvent event) {
+        Player player = event.getPlayer();
+        if (player != null && player.isOnline()) {
+            helpers.cacheOriginalLocation(player.getName(), player.getLocation());
+        }
     }
 }
